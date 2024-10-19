638e40239b8277ee424445cab25b753f77a9b2e0
Masato Iwaura <miwaura95@gmail.com>
2024-10-19 09:50:59 +0200

    Refactor: Update folder structure

--- app.js
+++ app.js
@@ -2,20 +2,25 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: './' });
+  res.render('index');
 });
 
 app.get('/login', (req, res) => {
-  res.sendFile('login.html', { root: './' });
+  res.render('login');
 });
 
 app.get('/register', (req, res) => {
-  res.sendFile('register.html', { root: './' });
+  res.render('register');
 });
 
+app.get('/cart', (req, res) => {
+  res.render('cart');
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
