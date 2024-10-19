dee87adc552f84a49db3b9019d4e0846e9a6560c
Masato Iwaura <miwaura95@gmail.com>
2024-10-19 09:58:20 +0200

    Refactor: add main.js

--- app.js
+++ app.js
@@ -5,22 +5,9 @@ const port = 3000;
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+const mainRouter = require('./routes/main');
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
-app.get('/login', (req, res) => {
-  res.render('login');
-});
-
-app.get('/register', (req, res) => {
-  res.render('register');
-});
-
-app.get('/cart', (req, res) => {
-  res.render('cart');
-});
+app.use('/', mainRouter);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
