f6ae54979f8f2677922f04c364f572836866f0b9
Masato Iwaura <miwaura95@gmail.com>
2024-11-01 13:31:33 +0200

    feature: register function

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000;
 
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 const mainRouter = require('./routes/main');
 
--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const dotenv = require("dotenv");
+const bcrypt = require('bcrypt');
+dotenv.config();
 const router = express.Router();
+
+const { Client } = require("pg");
+const client = new Client({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE_URL,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+});
+client.connect();
 
 router.get('/', (req, res) => {
   res.render('index');
@@ -9,8 +22,33 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.post('/login', (req, res) =>{
+    const {username, password} = req.body;
+    const query = {
+      text: "SELECT * FROM users WHERE username = $1 AND password = $2",
+      values: [username, password],
+    };
+    client.query(query).then((res) => {
+      console.log(res);
+    });
+    res.redirect('/');
+  });
+
 router.get('/register', (req, res) => {
   res.render('register');
+});
+
+router.post('/register', async (req, res) => {
+  const {username, password} = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const query = {
+    text: "INSERT INTO users (username, password) VALUES ($1, $2)",
+    values: [username, hashedPassword],
+  };
+  client.query(query).then((res) => {
+    console.log(res);
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
