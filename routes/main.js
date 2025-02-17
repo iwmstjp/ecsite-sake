9cf696fb469729a9624500b176174874648a4b00
iwmstjp <miwaura95@gmail.com>
2025-02-17 12:55:37 +0100

    add html pages

--- routes/main.js
+++ routes/main.js
@@ -4,18 +4,34 @@ const bcrypt = require('bcrypt');
 dotenv.config();
 const router = express.Router();
 
-const { Client } = require("pg");
-const client = new Client({
-  user: process.env.USER,
-  host: process.env.HOST,
-  database: process.env.DATABASE_URL,
-  password: process.env.PASSWORD,
-  port: process.env.PORT,
-});
-client.connect();
+// const { Client } = require("pg");
+// const client = new Client({
+//   user: process.env.USER,
+//   host: process.env.HOST,
+//   database: process.env.DATABASE_URL,
+//   password: process.env.PASSWORD,
+//   port: process.env.PORT,
+// });
+// client.connect();
 
 router.get('/', (req, res) => {
   res.render('index');
+});
+
+router.get('/item', (req, res) => {
+  res.render('item');
+});
+
+router.get('/cart', (req, res) => {
+  res.render('cart');
+});
+
+router.get('/order', (req, res) => {
+  res.render('order');
+});
+
+router.get('/success', (req, res) => {
+  res.render('success');
 });
 
 router.get('/login', (req, res) => {
@@ -24,30 +40,30 @@ router.get('/login', (req, res) => {
 
 router.post('/login', (req, res) =>{
     const {username, password} = req.body;
-    const query = {
-      text: "SELECT * FROM users WHERE username = $1 AND password = $2",
-      values: [username, password],
-    };
-    client.query(query).then((res) => {
-      console.log(res);
-    });
+    // const query = {
+    //   text: "SELECT * FROM users WHERE username = $1 AND password = $2",
+    //   values: [username, password],
+    // };
+    // client.query(query).then((res) => {
+    //   console.log(res);
+    // });
     res.redirect('/');
   });
 
-router.get('/register', (req, res) => {
-  res.render('register');
+router.get('/signup', (req, res) => {
+  res.render('signup');
 });
 
-router.post('/register', async (req, res) => {
+router.post('/signup', async (req, res) => {
   const {username, password} = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const query = {
-    text: "INSERT INTO users (username, password) VALUES ($1, $2)",
-    values: [username, hashedPassword],
-  };
-  client.query(query).then((res) => {
-    console.log(res);
-  });
+  // const hashedPassword = await bcrypt.hash(password, 10);
+  // const query = {
+  //   text: "INSERT INTO users (username, password) VALUES ($1, $2)",
+  //   values: [username, hashedPassword],
+  // };
+  // client.query(query).then((res) => {
+  //   console.log(res);
+  // });
   res.redirect('/');
 });
 
