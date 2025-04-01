e2f76798a51f6813b72faf9fc51e070e62d5ec4b
iwmstjp <miwaura95@gmail.com>
2025-04-01 21:05:29 +0200

    Feature: Update ranking functionality to include rank date in response and display

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -3,19 +3,20 @@ async function getTop10SakeId() {
     "https://muro.sakenowa.com/sakenowa-data/api/rankings"
   );
   const data = await response.json();
+  const rankDate = data.yearMonth;
   let top10 = [];
   for (let i = 0; i < 10; i++) {
     top10.push(data.overall[i].brandId);
   }
-  return top10;
+  return [top10, rankDate];
 }
 
-async function getTop10Sake(top10) {
+async function getTop10Sake(sakeIds) {
   const response = await fetch(
     "https://muro.sakenowa.com/sakenowa-data/api/brands"
   );
   const data = await response.json();
-  const top10Sake = data.brands.filter((item) => top10.includes(item.id));
+  const top10Sake = data.brands.filter((item) => sakeIds.includes(item.id));
   return top10Sake;
 }
 //{ id: 1, name: 'sake', breweryId: 1 }
